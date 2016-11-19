d877291fc06f0ef3c641d993752096e56a4b729d
Josh Given <josh.given@gmail.com>
2016-11-18 22:02:49 -0700

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,15 @@
+public class GasolineReceipt {
+  public static void main(String[] args) {
+    System.out.println("+------------------------+");
+    System.out.println("|                        |");
+    System.out.println("|      CORNER STORE      |");
+    System.out.println("| 2016-11-18  10:38PM    |");
+    System.out.println("|                        |");
+    System.out.println("| Gallons:       10.870  |");
+    System.out.println("| Price/gallon: $ 2.020  |");
+    System.out.println("|                        |");
+    System.out.println("| Fuel total:  $ 21.96   |");
+    System.out.println("|                        |");
+    System.out.println("+------------------------+");
+  }
+}
