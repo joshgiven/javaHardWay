217a5545e857c844ea5186ea8caf79d723094914
Josh Given <josh.given@gmail.com>
2016-11-18 21:28:05 -0700

    fix typo in exercise 1

--- FirstProg.java
+++ FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main(String[] args) {
-    System.out.println("Josh is dtermined to learn how to code.");
+    System.out.println("Josh is determined to learn how to code.");
     System.out.println("Today's date is November 13, 2016");
   }
 }
