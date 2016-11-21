32d3852dbf08f453ae16c2b27402e018e020a6ec
Josh Given <josh.given@gmail.com>
2016-11-20 18:38:50 -0700

    add study drills for exercise 8

--- RudeQuestions.java
+++ RudeQuestions.java
@@ -26,5 +26,23 @@ public class RudeQuestions {
 
     System.out.print("Well, thanks for answering my rude questions, ");
     System.out.println(name + ".");
+
+    String mamaInsult;
+    System.out.print("How fat is your mama? ");
+    mamaInsult = keyboard.next();
+    System.out.println("Dear " + name + "'s mama: " + name +
+                       "says you are \"" + mamaInsult + "\" fat.");
+
   }
 }
+
+// The program will not blow up if a integer is entered when a
+//   double is expected. An integer can easily be converted to
+//   a double
+
+// The program will not blow up if a number is entered when a
+//   string is expected. A string can contain numeric characters
+
+// Non-numeric values will blow the program up when ints or
+//    doubles are expected
+// An EOF (ctrl-D) will cause a blow up when strings are expected
